f1f3c574c5dec0959afb836a17b4676f8f0792c2
shubham <shubhamsain9640@gmail.com>
2024-07-17 00:23:05 +0530

    Update webpart.py

--- webpart.py
+++ webpart.py
@@ -83,7 +83,7 @@ background-size: cover;
 background-repeat: no-repeat;
 background-attachment: fixed;
 }}
-[id="disease-prediction-system"],[id="test-history"],[id="login"],[id="sign-up"][id="diabetes-prediction-using-ml"],[id="disease-prediction"],.st-emotion-cache-1wmy9hl e1f1d6gn1,.st-emotion-cache-vut4ln e1f1d6gn2{{
+[id="disease-prediction-system"],[id="test-history"],[id="login"],[id="sign-up"],[id="diabetes-prediction-using-ml"],[id="disease-prediction"]{{
     background:rgb(2,0,36);
     color:white;
     width:100%
